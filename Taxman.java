9798ae0db69b2e0b6c28fa41c240b73b457cbfa1
Landon Colburn <colburnlandon@gmail.com>
2020-02-24 10:59:27 -0600

    Inital

---
+++ Taxman.java
@@ -0,0 +1,16 @@
+/*
+  Landon Colburn
+  ©2019
+*/
+
+import java.util.*;
+import java.io.*;
+import java.awt.*;
+
+public class Taxman {
+
+  public static void main(String[] args) {
+    System.out.println("Hello world");
+  }
+
+}
